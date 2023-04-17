f49c9e3b877f31c241a82df81fe5722d4d43d4e0
Ananya Shah <shah.anan@northeastern.edu>
2023-04-17 21:46:52 +0000

    Emergency Contact Edit Get

---
+++ pages/Customer Purchase Edit/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,13 @@
+export default {
+	myFun2: () => {
+		{{GetDependentFirstName.run()}}
+		{{GetDependentLastName.run()}}
+		{{GetDependentsPhoneNumber.run()}}
+		{{GetDependentRelationship.run()}}
+		{{GetDependentStreet.run()}}
+		{{GetDependentsCity.run()}}
+		{{GetDependentState.run()}}
+		{{GetDependentZipcode.run()}}
+		{{GetDependentCountry.run()}}
+	},
+}
