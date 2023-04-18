9c1f8fd0f504d51348006417fd3a63cd83904a9c
Khanh Nguyen <nguyen.khanh1@northeastern.edu>
2023-04-18 03:13:29 +0000

    Put Method for customers/dependent route

--- pages/Customer Purchase Edit/jsobjects/JSObject1/JSObject1.js
+++ pages/Customer Purchase Edit/jsobjects/JSObject1/JSObject1.js
@@ -1,9 +1,5 @@
 export default {
 	myFun1: () => {
-		{{GetCustomersFirstName.run()}}
-		{{GetCustomersLastName.run()}}
-		{{GetCustomerPhone.run()}}
-		{{GetCustomerEmail.run()}}
-	
+		{{GetCustomerInfo.run()}};
 	},
 }
--- pages/Customer Purchase Edit/jsobjects/JSObject2/JSObject2.js
+++ pages/Customer Purchase Edit/jsobjects/JSObject2/JSObject2.js
@@ -1,13 +1,5 @@
 export default {
 	myFun2: () => {
-		{{GetDependentFirstName.run()}}
-		{{GetDependentInfo.run()}}
-		{{GetDependentsPhoneNumber.run()}}
-		{{GetDependentRelationship.run()}}
-		{{GetDependentStreet.run()}}
-		{{GetDependentsCity.run()}}
-		{{GetDependentState.run()}}
-		{{GetDependentZipcode.run()}}
-		{{GetDependentCountry.run()}}
+		{{GetDependentInfo.run()}};
 	},
 }
